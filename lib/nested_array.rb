683a32e84237b3d0151e00c68c4590779cf133ce
Rafael Salvador Valdez <manrvaldez.92@gmail.com>
2020-02-26 17:38:20 +0000

    Automatically backed up by Learn

--- lib/nested_array.rb
+++ lib/nested_array.rb
@@ -18,10 +18,8 @@ CONVENTIONAL_PRODUCE = [
 
 def assembled_matrix
 shelf =[
-  ORGANIC_PRODUCE[
-    CONVENTIONAL_PRODUCE
-    ]
-  ]
+  ORGANIC_PRODUCE[ ]
+  CONVENTIONAL_PRODUCE[ ]
 end
 
 def sorted_matrix
